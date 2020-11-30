29d7ea3a329ad22042808fcebed306bd01c05b1a
Carefree-Carmander <69743984+Carefree-Carmander@users.noreply.github.com>
2020-11-30 16:57:59 -0500

    Update script.js

--- src/script.js
+++ src/script.js
@@ -35,6 +35,20 @@ function formatDate(date) {
   return formatDate;
 }
 
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${hours}:${minutes}`;
+}
+
 console.log(formatDate(now));
 
 function search(event) {
